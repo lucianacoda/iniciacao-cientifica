557de2272dc086e125e537abc50d0a75b95d2a5d
Luciana Coda <lcsg23@hotmail.com>
2022-11-09 22:49:14 -0300

    feat: finalizes students register development

--- q2.py
+++ q2.py
@@ -1,12 +1,13 @@
-def cadastro_de_alunos ():
-
+def cadastro_de_alunos():
     opcao = 0
+    alunos = []
 
-    with open("lista_alunos.txt", "a+") as arquivo_alunos:
-        alunos = []
+    with open("lista_alunos.txt", "r+") as arquivo_alunos:
         linhas = arquivo_alunos.readlines()
+
         for linha in linhas:
-            alunos.append(dict(linha))
+            alunos.append(linha.replace("\n", ""))
+        print(alunos)
 
         print("Cadastro de Alunos: ")
         while not opcao == 3:
@@ -15,12 +16,12 @@ def cadastro_de_alunos ():
 
             if opcao == 1:
                 matricula = int(input("Digite a matricula do aluno: "))
-                nota1 = float(input("Digite a nota1:  "))
-                nota2 = float(input("Digite a nota2:  "))
-                nota3 = float(input("Digite a nota3:  "))
+                nota1 = float(input("Digite a nota1: "))
+                nota2 = float(input("Digite a nota2: "))
+                nota3 = float(input("Digite a nota3: "))
                 aluno = {
                     "matricula": matricula,
-                    "notas":  [
+                    "notas": [
                         nota1,
                         nota2,
                         nota3
@@ -35,9 +36,5 @@ def cadastro_de_alunos ():
         print("Finalizado.")
 
 
-
-
 if __name__ == "__main__":
-  cadastro_de_alunos()
-
-
+    cadastro_de_alunos()
